af5f99ce2483034f571d9fb7f2efe47b08be56e5
Arash Nemat Zadeh <arashnm80@gmail.com>
2023-06-12 10:23:30 +0330

    Create 11. Finding the percentage.py

---
+++ HackerRank/python-preparation/11. Finding the percentage.py
@@ -0,0 +1,12 @@
+if __name__ == '__main__':
+    n = int(input())
+    student_marks = {}
+    for _ in range(n):
+        name, *line = input().split()
+        scores = list(map(float, line))
+        student_marks[name] = scores
+    query_name = input()
+    
+    ans = sum(student_marks[query_name]) / 3
+    ans = "{:.2f}".format(ans)
+    print(ans)
