19fb00baf3c6916c404080d883e0017dc7703647
Arash Nemat Zadeh <arashnm80@gmail.com>
2023-07-18 18:08:53 +0330

    Create 34. Calendar Module.py

---
+++ HackerRank/python-preparation/34. Calendar Module.py
@@ -0,0 +1,7 @@
+import calendar
+
+month, day, year = map(int, input().split())
+weekday_index = calendar.weekday(year, month, day)
+weekday_name = calendar.day_name[weekday_index]
+
+print(weekday_name.upper())
